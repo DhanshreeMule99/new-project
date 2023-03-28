f214f63ccd78c12682caf56dd8b37b665513986a
Geexu <dhanashree@geexu.in>
2023-03-28 10:46:07 +0530

    riverpod state change and Textfield functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider((ref) => 0);
+final str = StateProvider((ref) => "2");
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,92 +32,182 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Home'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          child: const Text('Go to Counter Page'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: ((context) => CounterPage()),
+              ),
+            );
+          },
         ),
       ),
+    );
+  }
+}
+
+class CounterPage extends ConsumerWidget {
+  // const CounterPage({Key? key}) : super(key: key);key
+  var _formKey = GlobalKey<FormState>();
+  var isLoading = false;
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate();
+    if (!isValid!) {
+      return;
+    }
+    _formKey.currentState?.save();
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Using the WidgetRef to get the counter int from the counterProvider.
+    // The watch method makes the widget rebuild whenever the int changes value.
+    //   - something like setState() but automatic
+
+    TextEditingController mycontroller = TextEditingController();
+    final int counter = ref.watch(counterProvider);
+    final String validation = ref.watch(str);
+    final controller = ref.watch(str);
+    // ref.listen<String>(str, (previous, next) {
+    //   if (next == "4") {
+    //     showDialog(
+    //       context: context,
+    //       builder: (context) {
+    //         return AlertDialog(
+    //           title: Text('Warning'),
+    //           content: Text('its null'),
+    //           actions: [
+    //             TextButton(
+    //               onPressed: () {
+    //                 Navigator.of(context).pop();
+    //               },
+    //               child: Text('OK'),
+    //             )
+    //           ],
+    //         );
+    //       },
+    //     );
+    //   }
+    // });
+    ref.listen<int>(
+      counterProvider,
+      // "next" is referring to the new state.
+      // The "previous" state is sometimes useful for logic in the callback.
+      (previous, next) {
+        if (next >= 5) {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Warning'),
+                content:
+                    Text('Counter dangerously high. Consider resetting it.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('OK'),
+                  )
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.invalidate(counterProvider);
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+          IconButton(
+            onPressed: () {
+              ref.invalidate(str);
+            },
+            icon: const Icon(Icons.ac_unit_outlined),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextFormField(
+                  controller: mycontroller,
+                  onChanged: (text) {
+                    // mycontroller.text= text;
+
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter Your Name',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter a valid name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Text(
+                counter.toString(),
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              Text(
+                validation.toString(),
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ])),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          final isValid = _formKey.currentState?.validate();
+          if (!isValid!) {
+            return;
+          } else {
+            ref.read(str.state).state = mycontroller.text;
+            // Using the WidgetRef to read() the counterProvider just one time.
+            //   - unlike watch(), this will never rebuild the widget automatically
+            // We doxn't want to get the int but the actual StateNotifier, hence we access it.
+            // StateNotifier exposes the int which we can then mutate (in our case increment).
+
+            // ref
+            //     .read(str.notifier)
+            //     .state++;
+            ref.read(counterProvider.notifier).state++;
+          }
+        },
+      ),
     );
   }
 }
